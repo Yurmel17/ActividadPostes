0688771951386116002acb66db0f266f0de2487b
Yurmel <YURMEL1701@gmail.com>
2022-02-10 02:02:54 -0500

    final changes

--- conexiones.py
+++ conexiones.py
@@ -1,7 +1,34 @@
 import elementos as e
 
-poste1 = e.Poste(5, 1)
-poste2 = e.Poste(8, 2)
+#creacion de postes
+poste_1 = e.Poste(1, 5)
+poste_2 = e.Poste(2, 8)
+poste_3 = e.Poste(3, 5)
 
-poste2.reporte()
+#creacion de elementos
+transfromador_1 = e.Transformador(1)
+relay_1 = e.Relay(1)
+relay_2 = e.Relay(2)
+relay_3 = e.Relay(3)
 
+#creacion de lineas
+alta_tension = e.Linea(1)
+media_tension = e.Linea()
+
+#instalacion de elementos
+poste_3.instalacion(transfromador_1)
+poste_1.instalacion(relay_1)
+poste_2.instalacion(relay_2)
+poste_3.instalacion(relay_3)
+
+#conexion de postes
+poste_2.conexion(poste_1, media_tension)
+poste_2.conexion(poste_3, media_tension)
+poste_1.conexion(poste_3, alta_tension)
+poste_1.conexion(poste_3, media_tension)
+poste_3.conexion(poste_2, alta_tension)
+
+#impresion del estado de cada poste
+poste_1.reporte()
+poste_2.reporte()
+poste_3.reporte()
--- elementos.py
+++ elementos.py
@@ -2,32 +2,60 @@ import os
 
 class Poste:
 
-    contador = 0
+    conexiones = 0
+    instalaciones = 0
 
-    def __init__(self, num_apoyos, num_elemento) -> None:
-
-        self.id = num_elemento
-        file = open(f"estado_poste_{self.id}.txt", "w")
+    def __init__(self, num_elemento, num_apoyos) -> None:
+        self.num_elemento = num_elemento
+        file = open(f"estado_poste_{self.num_elemento}.txt", "w")
 
         if (num_apoyos == 5):
             self.nombre = "poste normal"
-            file.write(f"Se ha creado el elemento: <{self.nombre} {num_elemento}>" + os.linesep)
-            
+            self.apoyos = 5
+            file.write(f"Se ha creado el elemento: <{self.nombre} {self.num_elemento}>" + os.linesep)
         else:
             self.nombre = "poste grande"
-            file.write(f"Se ha creado un elemento: <{self.nombre} {num_elemento}>" + os.linesep)
+            self.apoyos = 8
+            file.write(f"Se ha creado un elemento: <{self.nombre} {self.num_elemento}>" + os.linesep)
 
         file.close()
 
-    def conexion(self, elemento):
-        print("a")
 
-    def instalacion(self):
-        print("a")
+    def conexion(self, elemento, linea):
+        file1 = open(f"estado_poste_{self.num_elemento}.txt", "a+")
+        file2 = open(f"estado_poste_{elemento.num_elemento}.txt", "a+")
+        
+        self.conexiones += 1
+        elemento.conexiones += 1
+
+        if (self.conexiones + self.instalaciones) < 6:
+            file1.write(f"Se ha creado una conexión entre el <{self.nombre} {self.num_elemento}> y el <{elemento.nombre} {elemento.num_elemento}> a traves de una {linea.nombre}" + os.linesep)
+            file2.write(f"Se ha creado una conexión entre el <{self.nombre} {self.num_elemento}> y el <{elemento.nombre} {elemento.num_elemento}> a traves de una {linea.nombre}" + os.linesep)        
+        else:
+            file1.write(f"El elemento <{self.nombre} {self.num_elemento}> se ha quedado sin apoyos y no acepta mas conexiones" + os.linesep)
+            self.conexiones -= 1
+            elemento.conexiones -= 1
+        
+        file1.close()
+        file2.close()
+
+    def instalacion(self, elemento):
+        file = open(f"estado_poste_{self.num_elemento}.txt", "a+")
+        self.instalaciones += 1
+       
+        if (self.conexiones + self.instalaciones) < 6:
+            file.write(f"Se ha instalado un <{elemento.nombre}> en el <{self.nombre} {self.num_elemento}>" + os.linesep)
+        else:
+            file.write(f"El elemento <{self.nombre} {self.num_elemento}> se ha quedado sin apoyos y no acepta mas conexiones" + os.linesep)
+       
+        file.close()
 
     def reporte(self):
-        file = open(f"estado_poste_{self.id}.txt", "r")
+        file = open(f"estado_poste_{self.num_elemento}.txt", "a+")
+        file.write(f"El elemento: <{self.nombre} {self.num_elemento}> tiene {self.apoyos - (self.conexiones + self.instalaciones)} apoyos libres")
+      
         print(file.read())
+        
         file.close()
 
 
@@ -35,12 +63,14 @@ class Transformador:
 
     def __init__(self, num_elemento) -> None:
         self.nombre = f"transformador {num_elemento}"
+        self.num_elemento = num_elemento
 
 
 class Relay:
 
     def __init__(self, num_elemento) -> None:
         self.nombre = f"relay {num_elemento}"
+        self.num_elemento = num_elemento
 
 
 class Linea:
@@ -48,7 +78,8 @@ class Linea:
     def __init__(self, tipo=0) -> None:
         
         if (tipo == 1):
-            self.nombre = "linea alta tension"
+            self.nombre = "linea de alta tension"
+            self.tipo = 1
         else:
-            self.nombre = "linea media tension"
-
+            self.nombre = "linea de media tension"
+            self.tipo = 0
